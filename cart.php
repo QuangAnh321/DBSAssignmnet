0e258b503de02db5efcd614c2741d957ce1ca589
QuangAnh321 <littledragon1998@gmail.com>
2019-05-11 16:51:30 +0700

    Fixed quantity and delete cart icon

--- cart.php
+++ cart.php
@@ -36,7 +36,7 @@ include('inc/header.php');
 							//create array of initail qty which is 1
 							$index = 0;
 							if (!isset($_SESSION['qty_array'])) {
-								$_SESSION['qty_array'] = array_fill(0, count($_SESSION['cart']), "1");
+								$_SESSION['qty_array'] = array_fill(0, count($_SESSION['cart']), 1);
 							}
 							$sql = "SELECT * FROM products WHERE product_id IN (" . implode(',', $_SESSION['cart']) . ")";
 							$query = mysqli_query($conn, $sql);
@@ -44,19 +44,21 @@ include('inc/header.php');
 								?>
 								<tr>
 									<td>
-										<a href="delete_item.php?id=<?php echo $row['product_id']; ?>&index=<?php echo $index; ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+									<a href="delete_item.php?id=<?php echo $row['product_id']; ?>&index=<?php echo $index; ?>">
+											<i class="fas fa-trash" ></i>
+									</a>	
 									</td>
 									<td><?php echo $row['product_name']; ?></td>
 									<td><?php echo number_format($row['product_price'], 2); ?></td>
 									<input type="hidden" name="indexes[]" value="<?php echo $index; ?>">
-									<td><input type="text" class="form-control" value="<?php echo $_SESSION['qty_array'][$index]; ?>" name="qty_<?php echo $index; ?>"></td>
+									<td><input type="number" class="form-control" value="<?php echo $_SESSION['qty_array'][$index]; ?>" name="qty_<?php echo $index; ?>" min="1" max="4"></td>
 									<td><?php echo number_format($_SESSION['qty_array'][$index] * $row['product_price'], 2); ?></td>
 									<?php $total += $_SESSION['qty_array'][$index] * $row['product_price']; ?>
 								</tr>
 								
 								<?php
 								$index++;
-								var_dump($_SESSION['qty_array']);
+								// var_dump($_SESSION['qty_array']);
 							}
 						} else {
 							?>
@@ -83,4 +85,10 @@ include('inc/header.php');
 </div>
 </div>
 </div>
+<script>
+	// Prevent user input on quantity field, use up and down button only
+	$("[type='number']").keypress(function (evt) {
+    evt.preventDefault();
+});
+</script>
 <?php include('inc/footer.php'); ?>
